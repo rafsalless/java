075351b5348f110ddeec1fe877a95fb21261d9b7
Rafsalless <rafaelasallescomer@gmail.com>
2023-09-12 20:19:32 -0300

    minha conta bancaria

--- conta-bancaria/conta-bancaria/src/ContaTerminal.java
+++ conta-bancaria/conta-bancaria/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-       
+    public static void main(String[] args) {
+       //Todo: Conhecer e importar a classe scanner
+
+       //Exibir as mensagens para o nosso usuário
+
+       //Obter pela scanner os valores digitados no terminal
+
+       //exibir a mensagem conta criada
+
+       try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+        double saldo = 526.85;
+
+        System.out.println("Por favor, digite o numero da Conta: ");
+            int numero = scanner.nextInt(0);
+
+        System.out.println("Por favor, digite o numero da Agencia: ");
+            String agencia = scanner.next();
+
+        System.out.println("Qual Seu nome completo?: ");
+            String nomeCompleto = scanner.next();
+
+            System.out.println("Aegncia: " + agencia);
+            System.out.println("Conta: " + numero);
+            System.out.println("Olá, " + nomeCompleto + "! O seu saldo atual é de: " + saldo);
+    }
+
+
     }
 }
